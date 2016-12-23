82e574c53cfe8a7380af538b158df88a1cfa3586
Vadim Matsishin <matsishin.vadim@gmail.com>
2016-12-23 18:40:04 +0300

    added ProgressDialogFragment with its llayout. Simple dialog fragment which allows to show circle progress bar in the center of screen

---
+++ ProgressDialogFragment/ProgressDialogFragment.java
@@ -0,0 +1,55 @@
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ru.razomovsky.wheelytask.R;
+
+/**
+ * Created by vadim on 22/10/16.
+ */
+
+public class ProgressDialogFragment extends DialogFragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(DialogFragment.STYLE_NO_TITLE, 0);
+    }
+
+    @Override
+    @NonNull
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Dialog dialog = super.onCreateDialog(savedInstanceState);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+        return dialog;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.progress_dialog, container, false);
+    }
+
+    @Override
+    public void dismiss() {
+        if (isAdded()) {
+            super.dismiss();
+        }
+    }
+
+    @Override
+    public void onDismiss(DialogInterface dialog) {
+        super.onDismiss(dialog);
+        Activity activity = getActivity();
+        if (activity != null && activity instanceof DialogInterface.OnDismissListener) {
+            ((DialogInterface.OnDismissListener) activity).onDismiss(dialog);
+        }
+    }
+}
